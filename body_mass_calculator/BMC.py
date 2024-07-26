933b15762d32eb5a511aae29b817735a657de0fa
Deivin_Or <baldeivin82@gmail.com>
2024-07-25 19:33:59 -0600

    "Prueba en procedimiento"

--- body_mass_calculator/BMC.py
+++ body_mass_calculator/BMC.py
@@ -1,1 +1,8 @@
 # Inicio de proyecto de masa corporal
+
+body_weight = float(input('Ingrese su peso en kilogramos: \n\t kg --> '))
+height = float(input('Ingrese su peso en metros: \n\t mt: --> '))
+
+imc = (body_weight / height ** 2)
+
+print(f"{imc:.2f}")
